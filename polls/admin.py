b947e3a80e9940dfb85d51d3684b16d698612c8f
jonathanau <jonathan.au@gmail.com>
2013-08-09 17:59:45 -0700

    Display choices using StackedInline

--- polls/admin.py
+++ polls/admin.py
@@ -1,10 +1,19 @@
 from django.contrib import admin
+from polls.models import Choice
 from polls.models import Poll
+
+
+class ChoiceInline(admin.StackedInline):
+  model = Choice
+  extra = 3
+
 
 class PollAdmin(admin.ModelAdmin):
   fieldsets = [
       (None,               {'fields': ['question']}),
       ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
       ]
+  inlines = [ChoiceInline]
+
 
 admin.site.register(Poll, PollAdmin)
