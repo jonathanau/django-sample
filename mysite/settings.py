78afea46473f81bcf7e87a21a4df4c735e301fe7
jonathanau <jonathan.au@gmail.com>
2013-08-09 17:39:05 -0700

    Fix path for sqlite3 db

--- mysite/settings.py
+++ mysite/settings.py
@@ -13,7 +13,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '/Users/jonathanau/sample/mysite/sqlite3.db', # Or path to database file if using sqlite3.
+        'NAME': '/Users/jonathanau/sample/django-sample/sqlite3.db', # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
         'USER': '',
         'PASSWORD': '',
