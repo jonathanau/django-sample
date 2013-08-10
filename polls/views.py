ab20b2f7ec0932eb933faefb83ad8e65224d01e0
jonathanau <jonathan.au@gmail.com>
2013-08-10 13:42:34 -0700

    Refactored Polls views to use render shortcut and added templates for detail, results, and vote views

--- polls/views.py
+++ polls/views.py
@@ -1,22 +1,21 @@
 # Create your views here.
-from django.http import HttpResponse
-from django.template import RequestContext, loader
+from django.shortcuts import render
 
 from polls.models import Poll
 
 def index(request):
-  latest_poll_list = Poll.objects.order_by('-pub_date')[:5]
-  template = loader.get_template('polls/index.html')
-  context = RequestContext(request, {
-    'latest_poll_list': latest_poll_list,
-    })
-  return HttpResponse(template.render(context))
+  latest_poll_list = Poll.objects.all().order_by('-pub_date')[:5]
+  context = {'latest_poll_list': latest_poll_list}
+  return render(request, 'polls/index.html', context)
 
 def detail(request, poll_id):
-  return HttpResponse("You're looking at poll %s." % poll_id)
+  context = {'poll_id': poll_id}
+  return render(request, 'polls/detail.html', context)
 
 def results(request, poll_id):
-  return HttpResponse("You're looking at the results of poll %s." % poll_id)
+  context = {'poll_id': poll_id}
+  return render(request, 'polls/results.html', context)
 
 def vote(request, poll_id):
-  return HttpResponse("You're voting on poll %s." % poll_id)
+  context = {'poll_id': poll_id}
+  return render(request, 'polls/vote.html', context)
