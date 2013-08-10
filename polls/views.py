171a05f4d14bf41fdd90640dd414941a24c4f065
jonathanau <jonathan.au@gmail.com>
2013-08-10 14:11:46 -0700

    Details view: Raise 404 if poll does not exist and display poll object

--- polls/views.py
+++ polls/views.py
@@ -1,4 +1,5 @@
 # Create your views here.
+from django.http import Http404
 from django.shortcuts import render
 
 from polls.models import Poll
@@ -8,13 +9,24 @@ def index(request):
   context = {'latest_poll_list': latest_poll_list}
   return render(request, 'polls/index.html', context)
 
+
 def detail(request, poll_id):
-  context = {'poll_id': poll_id}
+  try:
+    poll = Poll.objects.get(pk=poll_id)
+  except Poll.DoesNotExist:
+    raise Http404
+
+  context = {'poll_id': poll_id,
+      'poll': poll,
+      }
+
   return render(request, 'polls/detail.html', context)
+
 
 def results(request, poll_id):
   context = {'poll_id': poll_id}
   return render(request, 'polls/results.html', context)
+
 
 def vote(request, poll_id):
   context = {'poll_id': poll_id}
