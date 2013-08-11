fe60c5de5d5af719da5363bfe9434275cd8f3d64
jonathanau <jonathan.au@gmail.com>
2013-08-10 19:43:34 -0700

    Add tests for Polls.was_published_recently

--- polls/tests.py
+++ polls/tests.py
@@ -4,13 +4,38 @@ when you run "manage.py test".
 
 Replace this with more appropriate tests for your application.
 """
+import datetime
 
+from django.utils import timezone
 from django.test import TestCase
 
+from polls.models import Poll
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+
+class PollMethodTests(TestCase):
+
+  def test_was_published_recently_with_future_poll(self):
+    """
+    was_published_recently() should return False for polls whose
+    pub_date is in the future
+    """
+    future_poll = Poll(pub_date=timezone.now() + datetime.timedelta(days=1))
+    self.assertEqual(future_poll.was_published_recently(), False)
+
+
+  def test_was_published_recently_with_old_poll(self):
+    """
+    was_published_recently() should return False for polls whose pub_date
+    is older than 1 day
+    """
+    old_poll = Poll(pub_date=timezone.now() - datetime.timedelta(days=2))
+    self.assertEqual(old_poll.was_published_recently(), False)
+
+
+  def test_was_published_recently_with_recent_poll(self):
+    """
+    was_published_recently() should return True for polls whose pub_date
+    is within the last day
+    """
+    recent_poll = Poll(pub_date=timezone.now() - datetime.timedelta(hours=1))
+    self.assertEqual(recent_poll.was_published_recently(), True)
