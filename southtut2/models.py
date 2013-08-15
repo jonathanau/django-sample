3d5147ad27f5da4ca128aa5ba40cdd8db175c9bf
jonathanau <jonathan.au@gmail.com>
2013-08-15 13:35:09 -0700

    Add password_salt and password_hash to model

--- southtut2/models.py
+++ southtut2/models.py
@@ -1,6 +1,13 @@
 from django.db import models
+import sha
 
 class User(models.Model):
   username = models.CharField(max_length=255)
   password = models.CharField(max_length=60)
+  password_salt = models.CharField(max_length=8, null=True)
+  password_hash = models.CharField(max_length=40, null=True)
   name = models.TextField()
+
+  def check_password(self, password):
+    password_hash = sha.sha(self.password_salt + password).hexdigest()
+    return password_hash == self.password_hash
